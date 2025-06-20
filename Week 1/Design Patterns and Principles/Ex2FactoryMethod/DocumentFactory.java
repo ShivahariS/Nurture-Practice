6db5ce12d4267516b1f5cb66f3df7696f563fc73
Shivahari S <shivaharipvt@gmail.com>
2025-06-20 12:12:20 +0530

    Add files via upload

---
+++ Week 1/Design Patterns and Principles/Ex2FactoryMethod/DocumentFactory.java
@@ -0,0 +1,23 @@
+package Ex2FactoryMethod;
+
+public abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+class WordDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
