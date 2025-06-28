befe98e73d6d1810617f1fa600dabd8118105b69
Shivahari S <shivaharipvt@gmail.com>
2025-06-28 22:58:30 +0530

    Create Testclss.java

---
+++ Week 2_PLSQL Programming, TDD using JUnit5 and Mockito, SLF4J Logging Framework/JUnit, Mockito and SL4J/1. JUnit_Basic Testing Exercises/Ex1_Setting Up JUnit/Testclss.java
@@ -0,0 +1,9 @@
+package Ex1.JUnit5;
+import static org.junit.Assert.*;
+import org.junit. Test;
+public class Testclss {
+    @Test
+    public void testAdd() {
+        assertEquals(5, 5);
+    }
+}
