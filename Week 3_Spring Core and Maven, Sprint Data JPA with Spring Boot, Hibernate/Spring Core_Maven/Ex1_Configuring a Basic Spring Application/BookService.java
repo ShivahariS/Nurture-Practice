c3b2b5166ceb5da6e633fc775c41c8bd1ccb79b6
Shivahari S <shivaharipvt@gmail.com>
2025-07-05 16:46:10 +0530

    Create BookService.java

---
+++ Week 3_Spring Core and Maven, Sprint Data JPA with Spring Boot, Hibernate/Spring Core_Maven/Ex1_Configuring a Basic Spring Application/BookService.java
@@ -0,0 +1,14 @@
+package Ex1.LibraryManagement;
+
+public class BookService {
+	private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String title) {
+        System.out.println("Adding book: " + title);
+        bookRepository.saveBook(title);
+    }
+}
