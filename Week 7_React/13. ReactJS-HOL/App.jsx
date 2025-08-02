ac9e4edf607767b9918a9b53a148fbf4d2ba6c59
Shivahari S <shivaharipvt@gmail.com>
2025-08-02 22:28:23 +0530

    Create App.jsx

---
+++ Week 7_React/13. ReactJS-HOL/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CourseDetails from './Components/CourseDetails';
+import BookDetails from './Components/BookDetails';
+import BlogDetails from './Components/BlogDetails';
+import './App.css';
+function App() {
+  return (
+    <div className="container">
+      <div className="column no-border">
+        <CourseDetails />
+      </div>
+      <div className="column with-border">
+        <BookDetails />
+      </div>
+      <div className="column with-border">
+        <BlogDetails />
+      </div>
+    </div>
+  );
+}
